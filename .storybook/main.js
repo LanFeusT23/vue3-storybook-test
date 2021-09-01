31f546a60afd3d5ec593082bc1f578adbfb0c650
Jonathan Hankey <jhankey@blizzard.com>
2021-09-01 10:30:53 -0700

    Fixed storybook building

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,23 @@
-const custom = require("../node_modules/@vue/cli-service/webpack.config.js");
+const path = require("path");
 
 module.exports = {
     stories: [
         "../src/**/*.stories.mdx",
         "../src/**/*.stories.@(js|jsx|ts|tsx)",
     ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-postcss",
-    ],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
     webpackFinal: (config) => {
-        return {
-            ...config,
-            module: { ...config.module, rules: custom.module.rules },
-        };
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "postcss-loader",
+                "sass-loader",
+            ],
+            include: path.resolve(__dirname, "../"),
+        });
+
+        return config;
     },
 };
